c87578dde1ffbc95111e6ad0f0e5c83c3898b00b
Colin Kiama <colinkiama@hotmail.co.uk>
2018-05-28 23:30:07 +0100

    Add project files.

---
+++ ChangingGrid/Model/movingNum.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangingGrid.Model
+{
+    public class movingChar
+    {
+        public const char characterMoving = 'x';
+
+        public int[] charPosition = new int[2];
+
+        public movingChar(int startingX, int startingY)
+        {
+            charPosition[0] = startingX;
+            charPosition[1] = startingY;
+        }
+
+        internal void MoveForward()
+        {
+            if (charPosition[1] < 9)
+            {
+                charPosition[1] += 1;
+            }
+            else
+            {
+                if (charPosition[0] < 9)
+                {
+                    charPosition[0] += 1;
+                }
+
+                else
+                {
+                    charPosition[0] = 0;
+                }
+
+                charPosition[1] = 0;
+            }
+            
+        }
+
+        internal void MoveBack()
+        {
+            if (charPosition[1] > 0)
+            {
+                charPosition[1] -= 1;
+            }
+            else
+            {
+                if (charPosition[0] > 0)
+                {
+                    charPosition[0] -= 1;
+                }
+
+                else
+                {
+                    charPosition[0] = 9;
+                }
+
+                charPosition[1] = 9;
+            }
+        }
+
+        internal void MoveUp()
+        {
+            if (charPosition[0] > 0)
+            {
+                charPosition[0] -= 1;
+            }
+            else
+            {
+                charPosition[0] = 9;
+            }
+        }
+
+        internal void MoveDown()
+        {
+            if (charPosition[0] < 9)
+            {
+                charPosition[0] += 1;
+            }
+            else
+            {
+                charPosition[0] = 0;
+            }
+        }
+    }
+}
---
+++ ChangingGrid/Program.cs
@@ -0,0 +1,148 @@
+﻿using ChangingGrid.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangingGrid
+{
+    class Program
+    {
+        static char[,] grid = new char[10, 10];
+        static movingChar movingCharacter;
+        static void Main(string[] args)
+        {
+            CreateGrid();
+
+            ConsoleKeyInfo keyInfo;
+            while (true)
+            {
+                keyInfo = Console.ReadKey();
+                switch (keyInfo.Key)
+                {
+
+                    case ConsoleKey.LeftArrow:
+                        MoveCharBack();
+                        UpdateGrid();
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        MoveCharForward();
+                        UpdateGrid();
+                        break;
+
+                    case ConsoleKey.UpArrow:
+                        MoveCharUp();
+                        UpdateGrid();
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        MoveCharDown();
+                        UpdateGrid();
+                        break;
+                }
+
+
+
+            }
+
+        }
+
+        private static void MoveCharForward()
+        {
+            RevertChangesToCurrentPosition();
+            movingCharacter.MoveForward();
+        }
+
+        private static void MoveCharDown()
+        {
+            movingCharacter.MoveDown();
+            RevertChangesToCurrentPosition();
+        }
+
+        private static void UpdateGrid()
+        {
+            Console.Clear();
+
+            ShowTutorial();
+            int[] movingNumPosition = movingCharacter.charPosition;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (movingNumPosition[0] == i && movingNumPosition[1] == j)
+                    {
+                        grid[i, j] = movingChar.characterMoving;
+                    }
+
+                    else
+                    {
+                        grid[i, j] = '-';
+                    }
+                    Console.Write(grid[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void RevertChangesToCurrentPosition()
+        {
+            int[] position = movingCharacter.charPosition;
+           
+
+            grid[position[0], position[1]] = '-';
+        }
+
+        private static void MoveCharUp()
+        {
+            RevertChangesToCurrentPosition();
+            movingCharacter.MoveUp();
+
+
+        }
+
+
+        private static void MoveCharBack()
+        {
+            RevertChangesToCurrentPosition();
+            movingCharacter.MoveBack();
+        }
+
+       
+
+        static void ReadConsole()
+        {
+            Console.ReadLine();
+        }
+
+        private static void CreateGrid()
+        {
+            ShowTutorial();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                   
+                    if (i == 0 && j == 0)
+                    {
+                        grid[i, j] = movingChar.characterMoving;
+                        movingCharacter = new movingChar(i, j);
+                    }
+                    else
+                    {
+                        grid[i, j] = '-';
+                    }
+                    Console.Write(grid[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void ShowTutorial()
+        {
+            Console.WriteLine("Use the arrow keys to move the 'x'");
+            Console.WriteLine();
+        }
+    }
+}
